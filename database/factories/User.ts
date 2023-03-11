9cd4690dbb393396a184dd67c1a73da4c45441c6
Felipe Gangrel <felipefrog@gmail.com>
2023-03-11 11:40:42 -0300

    Creates factory and seeder for users

---
+++ database/factories/User.ts
@@ -0,0 +1,20 @@
+import Factory from '@ioc:Adonis/Lucid/Factory'
+import User from 'App/Models/User'
+import { UserRole } from 'Contracts/common'
+
+export const UserFactory = Factory.define(User, async ({ faker }) => {
+  const firstName = faker.name.firstName()
+  const lastName = faker.name.lastName()
+  const email = faker.internet.email(firstName, lastName).toLowerCase()
+
+  return {
+    firstName,
+    lastName,
+    email,
+    password: '123456',
+  }
+})
+  .state('AdminRole', (user) => {
+    user.role = UserRole.ADMIN
+  })
+  .build()
---
+++ database/seeders/User.ts
@@ -0,0 +1,27 @@
+import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
+import User from 'App/Models/User'
+import { UserRole } from 'Contracts/common'
+import { UserFactory } from 'Database/factories/User'
+
+type AdminEntry = Pick<User, 'firstName' | 'lastName' | 'email' | 'role'>
+
+const admins: AdminEntry[] = [
+  {
+    firstName: 'Suporte',
+    lastName: 'Clock In',
+    email: 'admin@example.com',
+    role: UserRole.ADMIN,
+  },
+]
+
+export default class UserSeeder extends BaseSeeder {
+  public async run() {
+    await User.truncate()
+
+    await UserFactory.merge(admins)
+      .apply('AdminRole')
+      .createMany(admins.length + 4)
+
+    await UserFactory.createMany(10)
+  }
+}
