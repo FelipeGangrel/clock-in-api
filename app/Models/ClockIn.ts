997d462896c9c9b35f2a9cff724915e60e2bcbf8
Felipe Oliveira <felipe.oliveira@mereo.com>
2023-05-24 15:05:15 -0300

    Creates model, factory and seeder for clock-ins

---
+++ app/Models/ClockIn.ts
@@ -0,0 +1,51 @@
+import { DateTime } from 'luxon'
+import { BelongsTo, belongsTo, column, computed } from '@ioc:Adonis/Lucid/Orm'
+import { ClockInType } from 'App/Contracts/ClockIns'
+import BaseModel from './BaseModel'
+import User from './User'
+
+export default class ClockIn extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public type: ClockInType
+
+  @column()
+  public userId: number
+
+  @belongsTo(() => User)
+  public user: BelongsTo<typeof User>
+
+  @column()
+  public latitude: string
+
+  @column()
+  public longitude: string
+
+  @computed()
+  public get location() {
+    return [this.latitude, this.longitude]
+  }
+
+  @column.dateTime({ autoCreate: true })
+  public dateTime: DateTime
+
+  @column()
+  public comment: string | null
+
+  @column()
+  public validatedBy: number | null
+
+  @column.dateTime()
+  public validatedAt: DateTime | null
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({
+    autoCreate: true,
+    autoUpdate: true,
+  })
+  public updatedAt: DateTime
+}
---
+++ database/seeders/ClockIn.ts
@@ -0,0 +1,11 @@
+import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
+import ClockIn from 'App/Models/ClockIn'
+import { ClockInFactory } from 'Database/factories/ClockIn'
+
+export default class ClockInSeeder extends BaseSeeder {
+  public async run() {
+    await ClockIn.truncate()
+
+    await ClockInFactory.createMany(10)
+  }
+}
